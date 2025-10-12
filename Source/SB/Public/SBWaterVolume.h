8801302a7fbdfcb05915fd5b5cea44a32293500a
wschseng <36938113+wschseng@users.noreply.github.com>
2025-10-12 10:38:57 +0200

    added SBWaterVolume

---
+++ Source/SB/Public/SBWaterVolume.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PhysicsVolume.h"
+#include "SBWaterVolume.generated.h"
+
+UCLASS(Blueprintable)
+class SB_API ASBWaterVolume : public APhysicsVolume
+{
+	GENERATED_BODY()
+	
+};
